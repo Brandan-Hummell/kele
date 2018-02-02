a8a47caa086582faea005865e4b3d316b0eedd56
Brandan <bh2df@virginia.edu>
2018-02-02 15:32:34 -0500

    Checkpoint 6: Can Retrieve messages, need to fix the create messages function.

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,12 +9,12 @@ class Kele
    
    def initialize(email, password)
     @base_url = 'https://www.bloc.io/api/v1'
-
+    @email = email
     response = Kele.post(
 			"#{@base_url}/sessions",
       body: { email: email, password: password }
     )
-
+    
     if response && response["auth_token"]
       @auth_token = response["auth_token"]
       puts "#{email} has sucessfully logged in"
@@ -32,6 +32,18 @@ class Kele
     url = 'https://www.bloc.io/api/v1/mentors/' + id.to_s + '/student_availability'
     response = self.class.get( url, values: { "id" => id }, headers: { "authorization" => @auth_token })
     @mentor_availability = JSON.parse(response.body.to_s)
+  end
+
+  def get_messages(page = 0)
+    url = 'https://www.bloc.io/api/v1/message_threads'
+    response = self.class.get( url, values: { "page" => page }, headers: { "authorization" => @auth_token })
+    @messages = JSON.parse(response.body.to_s)
+  end
+
+  def create_message(recipient_id, subject, body)
+    url = 'https://www.bloc.io/api/v1/messages'
+    response = Kele.post( url, values: { "sender" => @email, "recipient_id" => recipient_id, "subject" => subject, "stripped-text" => body }, headers: { "authorization" => @auth_token })
+    puts response
   end
 
 end
