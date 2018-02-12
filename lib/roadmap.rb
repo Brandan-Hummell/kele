6db5bf0108ad93060ae34c9a94ec369b89e0c14e
Brandan <bh2df@virginia.edu>
2018-02-11 19:02:16 -0500

    Checkpoint 7: Added checkpoint submitting functionality

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -10,4 +10,13 @@ module Roadmap
         response = self.class.get( url, values: { "id" => id }, headers: { "authorization" => @auth_token })
         @checkpoint = JSON.parse(response.body.to_s)
       end
+
+      def create_checkpoint(assignment_branch, assignment_commit_link, checkpoint_id, comment="No Comment")
+        me_response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+        me = JSON.parse(me_response.body.to_s)
+        enrollment_id = me["current_enrollment"]["id"]
+        p enrollment_id
+        url = 'https://www.bloc.io/api/v1/checkpoint_submissions'
+        response = self.class.post( url, body: { "assignment_branch" => assignment_branch, "assignment_commit_link" => assignment_commit_link, "checkpoint_id" => checkpoint_id, "comment" => comment, "enrollment_id" => enrollment_id }, headers: { "authorization" => @auth_token })
+      end
 end
